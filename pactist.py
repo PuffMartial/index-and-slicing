000815267bf28d66c260505e44bc2e0cae40d2b9
Psalms_Martial <109070870+PuffMartial@users.noreply.github.com>
2024-01-18 09:14:01 +0200

    Add files via upload

---
+++ pactist.py
@@ -0,0 +1,16 @@
+# index
+name = "Puff marshal"
+
+firt_name = name[:3]
+last_name = name[4:]
+funky_name = name[0:9:4]
+reverse_name = name[::-1]
+
+print(reverse_name)
+
+# slicing
+
+website = "info@cfg.com"
+
+slice = slice(7,-4)
+print(website[slice])
